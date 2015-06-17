7a6c6bfad9f09054dd3533c24bcdf1d4151148db
reducedtobinary <qinglan.zhou@gmail.com>
2015-06-17 12:38:30 +0800

    Initial commit

---
+++ js/controllers/MainController.js
@@ -0,0 +1,23 @@
+app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+  $scope.getGitInfo = function () {
+      $scope.userNotFound = false;
+      $scope.loaded = false;
+
+      $http.get("https://api.github.com/users/" +$scope.username)
+        .success(function(data) {
+	   $scope.user = data;
+           $scope.loaded = true;
+        })
+        .error(function () {
+		$scope.userNotFound = true;
+	})
+
+      $http.get("https://api.github.com/users/" + $scope.username + "/repos")
+	.success(function (data) {
+		$scope.repos = data;
+		$scope.reposFound = data.length ;
+	});
+
+      }
+
+}]);
